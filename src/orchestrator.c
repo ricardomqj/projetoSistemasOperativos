11116d0efe992459743676bb2efc53b15a4e4b01
Ricardo Jesus <92188713+ricardomqj@users.noreply.github.com>
2024-05-07 22:41:01 +0100

    pipeline

--- src/orchestrator.c
+++ src/orchestrator.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+ #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/time.h>
 
 #define MAX_SIZE 300
 #define NOT_EXECUTED 11
@@ -24,6 +25,8 @@ typedef struct package {
 	pid_t pid;
 	int status;
 	int command_type;
+	int expected_time;
+	long timestamp;
 	struct package *next;
 } Package;
 
@@ -34,7 +37,7 @@ typedef struct client {
 } Client;
 
 
-void update_package_status(Client **head, int status, int id) {
+void update_package_status(Client **head, int status, int id, long timestamp) {
 	Client *current = *head;
 	int updated = 0;
 	while(current != NULL && !updated) {
@@ -43,6 +46,7 @@ void update_package_status(Client **head, int status, int id) {
 			if(currentPackage->id == id) {
 				updated = 1;
 				currentPackage->status = status;
+				currentPackage->timestamp = timestamp;
 			}
 		}
 		current = current->next;
@@ -66,6 +70,8 @@ void add_package(Client **head, Package pack, int currentId) {
 			new_package->pid = pack.pid;
 			new_package->status = NOT_EXECUTED;
 			new_package->command_type = pack.command_type;
+			new_package->expected_time = pack.expected_time;
+			new_package->timestamp = pack.timestamp;
 			new_package->next = current->packages;
 			current->packages = new_package;
 			return;
@@ -86,6 +92,8 @@ void add_package(Client **head, Package pack, int currentId) {
 	new_package->pid = pack.pid;
 	new_package->status = NOT_EXECUTED;
 	new_package->command_type = pack.command_type;
+	new_package->expected_time = pack.expected_time;
+	new_package->timestamp = pack.timestamp;
 	new_package->next = NULL;
 	new_node->packages = new_package;
 	new_node->client_pid = pack.pid;
@@ -110,6 +118,8 @@ void print_clients(Client *head) {
 			printf("Id: %d\n", current_package->id);
 			printf("Status: %d\n", current_package->status);
 			printf("Command Type: %d\n", current_package->command_type);
+			printf("Expected Time: %d\n", current_package->expected_time);
+			printf("Timestamp: %ld\n", current_package->timestamp);
 			printf("____________________\n");
 			current_package = current_package->next;
 			i++;
@@ -155,9 +165,10 @@ void exec_command(int N, char* command, int task_id){
         printf("cmd[%d] -> %s\n", k, cmd[k]);
     }
 
-    char output_filename[32];
-    snprintf(output_filename, sizeof(output_filename), "output_folder/out%d.txt", task_id);
-    int fdOut = open(output_filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    char output_filename[64];
+    snprintf(output_filename, 64, "output_folder/out%d.txt", task_id);
+    printf("filename -> %s\n", output_filename);
+	int fdOut = open(output_filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
     if(fdOut == -1){
         perror("open");
@@ -175,96 +186,104 @@ void exec_command(int N, char* command, int task_id){
 	_exit(1);
 }
 
-// Funçãp que divide string do comando em comandos separados
-int split_cmds(char *input, char *cmds[]) {
-	int i = 0;
-	char *token = strtok(input, "|");
-	while(token != NULL) {
-		cmds[i++] = token;
-		token = strtok(NULL, "|");
-	}
-	return i;
-}
 
-// função que divide cada comando nos seus argumentos
-void parse_args(char *cmd, char *args[]) {
-	int i = 0;
-	char *token = strtok(cmd, " ");
-	while(token != NULL) {
-		args[i++] = token;
-		token = strtok(NULL, " ");
-	}
-	args[i] = NULL;
-}
+void parse_and_exec(char *cmd, int in_fd, int out_fd) {
+    char *args[10];  
+    int i = 0;
 
-void exec_pipelining(char *input, int task_id) {
-	int num_cmds;
-	char *cmds[MAX_CMDS];
-	int pipes[MAX_CMDS][2];
+    while (*cmd && i < 10) {
+        while (*cmd == ' ') cmd++; 
 
-	num_cmds = split_cmds(input, cmds);
+        if (*cmd == '\'') {  
+            cmd++;
+            args[i++] = cmd;
+            while (*cmd && *cmd != '\'') cmd++;
+            if (*cmd) *cmd++ = '\0';
+        } else {
+            args[i++] = cmd;
+            while (*cmd && *cmd != ' ') cmd++;
+            if (*cmd) *cmd++ = '\0';
+        }
+    }
+    args[i] = NULL;
 
-	char output_filename[32];
-    snprintf(output_filename, sizeof(output_filename), "output_folder/out%d.txt", task_id);
-    int fdOut = open(output_filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-
-    if(fdOut == -1){
-        perror("open");
-        _exit(1);
+    if (in_fd != STDIN_FILENO) {
+        dup2(in_fd, STDIN_FILENO);
+        close(in_fd);
+    }
+    if (out_fd != STDOUT_FILENO) {
+        dup2(out_fd, STDOUT_FILENO);
+        close(out_fd);
     }
 
-	for(int i = 0; i < num_cmds; i++) {
-		if(i < num_cmds - 1) {
-			if(pipe(pipes[i]) == -1) {
-				perror("pipe");
-				_exit(1);
-			}
-		}
+    execvp(args[0], args);
+    perror("execvp");
+    _exit(1);
+}
 
-		int pid = fork();
-		if(pid < 0) {
-			perror("fork");
-			_exit(1);
-		}
+void exec_pipelining(char *command, int task_id) {
+    int num_pipes = 0;
+    char *cmd = command;
+    char *tmp;
 
-		if(pid == 0) { // processo filho
-			if(i > 0) {
-				dup2(pipes[i-1][0], STDIN_FILENO);
-				close(pipes[i-1][0]);
-				close(pipes[i-1][1]);
-			}
-			if(i < num_cmds - 1) {
-				dup2(pipes[i][1], STDOUT_FILENO);
-				close(pipes[i][0]);
-				close(pipes[i][1]);
-			}
-			char *args[MAX_ARGS];
-			if(dup2(fdOut, STDOUT_FILENO) == -1 || dup2(fdOut, STDERR_FILENO) == -1){
-        		perror("dup2");
-        		_exit(1);
-    		}
-    		close(fdOut);
-			parse_args(cmds[i], args);
-			execvp(args[0], args);
-			perror("execvp");
-			_exit(1);
-		} else {
-			if(i > 0) {
-				close(pipes[i-1][0]);
-				close(pipes[i-1][1]);
-			}
-		}
-	}
+    while ((tmp = strchr(cmd, '|'))) {
+        num_pipes++;
+        cmd = tmp + 1;
+    }
 
-	for(int i = 0; i < num_cmds; i++) {
-		wait(NULL);
-	}
-	close(fdOut);
+    int fd[2], in_fd = 0;
+    int final_out_fd;
+
+    cmd = command;
+    for (int i = 0; i <= num_pipes; ++i) {
+        if (i != num_pipes) {
+            if (pipe(fd) == -1) {
+                perror("pipe");
+                _exit(1);
+            }
+        } else {
+            char filename[256];
+            snprintf(filename, sizeof(filename), "output_folder/%d.txt", task_id);
+            final_out_fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (final_out_fd == -1) {
+                perror("open file");
+                _exit(1);
+            }
+        }
+
+        tmp = strchr(cmd, '|');
+        if (tmp) *tmp = '\0';
+
+        int pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            _exit(1);
+        } else if (pid == 0) {
+            if (i != num_pipes) {
+                close(fd[0]);
+                parse_and_exec(cmd, in_fd, fd[1]);
+            } else {
+                parse_and_exec(cmd, in_fd, final_out_fd);
+            }
+        }
+
+        if (in_fd != 0) close(in_fd);
+        if (i != num_pipes) {
+            close(fd[1]);
+            in_fd = fd[0];
+        } else {
+            close(final_out_fd);
+        }
+
+        cmd = tmp + 1;
+    }
+
+    while (wait(NULL) > 0);
 }
 
 int main(int argc, char *argv[])
 {
-	
+	struct timeval start, end;
 	int N = 0;
 	int id = 0; // variável usada para associar um id aos clientes
 	pid_t pid;
@@ -278,7 +297,7 @@ int main(int argc, char *argv[])
 
 	int fdFifoCliOrch = open("tmp/fifoCliOrch", O_RDONLY);
 	/*
-    int fdOut = open("out.txt", O_WRONLY| O_CREAT | O_TRUNC, 0644);
+    int fdOut = open("tmp/out.txt", O_WRONLY| O_CREAT | O_TRUNC, 0644);
 	if(fdOut == -1){
 		perror("open");
 		_exit(1);
@@ -290,13 +309,8 @@ int main(int argc, char *argv[])
 		bytes_read = read(fdFifoCliOrch, &pack, sizeof(Package));
 		if(bytes_read > 0) {
 			pid_t pid_client = pack.pid;
-			printf("Estrutura do pack recebido: \n");
-			printf("pack.id -> %d\n", pack.id);
-			printf("pack.command -> %s\n", pack.command);
-			printf("pack.pid -> %d\n", pack.pid);
-			printf("pack.command_type -> %d\n", pack.command_type);
 			id++;
-			
+			gettimeofday(&start, NULL);
 			int pipefd[2];
 			if(pipe(pipefd) == -1) {
 				perror("pipe");
@@ -372,31 +386,28 @@ int main(int argc, char *argv[])
 				close(pipefd[1]);
 				Package pack_buffer;
 				int status;
-				printf("[processo-pai] vou ler do pipe\n");
 				read(pipefd[0], &pack_buffer, sizeof(Package));
 				close(pipefd[0]);
-				printf("vou adicionar o package ao client_list\n");
 				add_package(&client_list, pack_buffer, id);
 				printf("A imprimir os pacotes dos clientes...\n");
-				print_clients(client_list);
-				printf("depois do print_clients\n");
 				waitpid(pid, &status, 0);
 				if(WIFEXITED(status) && WEXITSTATUS(status) == 0) {
 					// o client list só não é null para o processo filho
-					update_package_status(&client_list, EXECUTED, id);
-					printf("Estado do package alterado para EXECUTED!\n");
-					print_clients(client_list);
-					printf("depois do print_clients após alteração do status do programa\n");
+					gettimeofday(&end, NULL);
+					long seconds = (end.tv_sec - start.tv_sec); // Diferença de segundos
+					long micros = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec); // Diferença total em microssegundos
+					long millis = micros / 1000; // Conversão para milissegundos
+					update_package_status(&client_list, EXECUTED, id, millis);
+				
 				} else {
-					update_package_status(&client_list, EXECUTED_ERROR, id);
-					printf("Estado do package alterado para EXECUTED_ERROR!\n");
-					printf("A imprimir os pacotes dos clientes...\n");
-					print_clients(client_list);
+					update_package_status(&client_list, EXECUTED_ERROR, id, 0);
 				}
 				close(pipefd[0]);
 			}
+		//close(fdOut);
 		}
+	//close(fdOut);
 	}
-	unlink("fifoCliOrch");
+	//unlink("tmp/fifoCliOrch");
 	return 0;
 }
